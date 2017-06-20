d6c38fd7702de262c05ea9889b70e2d11df9113b
Danijela Mikulicic <danijela2405@gmail.com>
2017-06-20 23:46:36 +0200

    toDoApp: added fixtures loading on setUp()

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -2,19 +2,33 @@
 
 namespace Tests\AppBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Console\Input\StringInput;
 
 class TaskControllerTest extends WebTestCase
 {
+    public function setUp()
+    {
+        $kernel = new \AppKernel('test', true);
+        $kernel->boot();
+        $application = new Application($kernel);
+        $application->run(new StringInput('doctrine:fixtures:load --no-interaction'));
+        return;
+    }
+
     public function testIndex()
     {
-        $client = static::createClient(array(), array(
-            'PHP_AUTH_USER' => 'username',
-            'PHP_AUTH_PW'   => 'pa$$word',
-        ));
+        $client = static::createClient(
+            array(),
+            array(
+                'PHP_AUTH_USER' => 'duje',
+                'PHP_AUTH_PW' => 'duje',
+            )
+        );
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
         $this->assertContains('You don\'t have any lists yet.', $crawler->filter('#container')->text());
     }
+
 }
